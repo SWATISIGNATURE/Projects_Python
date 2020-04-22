d5ff233601b02e8de7f2a0d42020355dc69b3824
swatisignature <swatimishra.puri@gmail.com>
2020-04-22 11:32:51 -0700

    Directory restructuring

---
+++ Data_Structures/Heaps.py
@@ -0,0 +1,12 @@
+"""
+Heaps:
+    1. used to maintain a maximum or minimum value in a dataset.Heaps tracking the maximum or minimum value are max-heaps or min-heaps.
+    2. heaps are commonly used to create a priority queue.
+    3. min Heap has the two qualities: The root is the minimum value of the dataset. Every child’s value is greater than its parent.
+    4. max Heap has the two qualities: The root is the maximum value of the dataset. Every child’s value is less than its parent.
+        left child: (index * 2) + 1
+        right child: (index * 2) + 2
+        parent: (index - 1) / 2 — not used on the root!
+
+
+"""
